7eb1719a22fc4e622f4fba13f65cfafd94e17585
Lucas Bassett-Audain <lbassettaudain@ufl.edu>
2015-12-01 14:19:18 -0500

    BOARD: Add Jeff's Board code

---
+++ chess.cpp
@@ -0,0 +1,50 @@
+#include "chess.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+chessBoard newBoard;
+
+
+int main(){
+    int choice;
+    bool exit = false;
+    //There's going to be a menu
+    cout << "MAIN MENU: " << "\n\n";
+    cout << "Please select an option (1 - ): " << endl;
+    cin >> choice;
+
+    while(exit != true){
+        if (choice == 1){
+            newBoard.getBoard();
+        }
+        int i = 0;
+        int j = 0;
+        piece newPawn1(newBoard,"pawn",i,j++);
+        piece newPawn2(newBoard,"pawn",i,j++);
+        piece newPawn3(newBoard,"pawn",i,j++);
+        piece newPawn4(newBoard,"pawn",i,j++);
+        piece newPawn5(newBoard,"pawn",i,j++);
+        piece newPawn6(newBoard,"pawn",i,j++);
+        piece newPawn7(newBoard,"pawn",i,j++);
+        piece newPawn8(newBoard,"pawn",i,j++);
+
+        cin >> choice;
+        if (choice == 2){
+            newBoard.getBoard();
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            int l = 0;
+            cin >> i >> j >> k >> l;
+            newBoard.movePiece(i,j,k,l);
+            newBoard.getBoard();
+
+        }
+        else{
+            exit = true;
+        }
+    }
+    return 0;
+}
---
+++ chess.h
@@ -0,0 +1,77 @@
+#ifndef CHESS_H
+#define CHESS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class chessBoard{
+    private:
+        string board [8] [8];
+
+    public:
+        chessBoard(){
+            for (int row = 0; row < 8; row++){
+                for (int column = 0; column < 8; column++){
+                    board [row] [column] = "SPACE";
+                }
+            }
+        };
+
+        void getBoard(){
+            for (int i = 0; i < 8; i++){
+                for (int j = 0; j < 8; j++){
+                    cout << board[i][j] << "\t";
+                }
+                cout << "\n\n\n";
+            }
+        };
+
+        string getBoardPiece(int row, int column){
+            return board [row] [column];
+        };
+
+        void setBoardPiece(string name, int row, int column){
+            board [row] [column] = name;
+        };
+
+        void movePiece(int i, int j, int k, int l){
+            board [k][l] = board [i] [j];
+            board [i] [j] = "SPACE";
+        };
+};
+
+class piece: public chessBoard{
+    private:
+        string nameOfPiece;
+    public:
+        piece(chessBoard &newBoard, string name, int row, int column){
+            nameOfPiece = name;
+            newBoard.setBoardPiece(nameOfPiece, row, column);
+        };
+};
+/*    public:
+        checkPiece(int row, int column){
+            if (board [row] [column] == "pawn"){
+
+            }
+        };
+};*/
+
+/*
+class pawnPiece: public chessBoard{
+    private:
+        string pawn;
+
+    public:
+        pawnPiece(chessBoard &setNewBoard, int row, int column){
+            setNewBoard.setBoardPiece("pawn", row, column);
+        };
+
+        void movePawn(){
+
+        };
+};*/
+
+#endif
