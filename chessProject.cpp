35f4699c9b61e3c068346d573429025ae1103234
jthornbeck <jthornbeck@ufl.edu>
2015-12-02 23:12:17 +0000

    ANother update

--- chessProject.cpp
+++ chessProject.cpp
@@ -10,6 +10,22 @@ chessBoard newBoard;
 int main(){
 	bool exit = false;
 	int menuSelection;
+	
+	while (exit != true){
+		cout << "MAIN MENU" << endl;
+		cout << "Please select an option (1 - ). Type 0 for exit. Choice: " << endl;
+		cout << "1: A game of chess" << endl;
+		cin >> menuSelection;
+		if (menuSelection == 0){
+			exit = true;
+		}
+		else if (menuSelection == 1){
+			
+		}
+		else{
+			cout << "Please enter a valid choice. Try again." << endl;
+		}
+	}
 	newBoard.getBoard();
 	newBoard.setUpBoard();
 	newBoard.getBoard();
